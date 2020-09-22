c86a56fd740ccc322e3f894e8316478db66ba1ef
Andrew Owen <andrew.owen@willowtreeapps.com>
2020-09-22 10:17:27 -0400

    add typography global styles and add woff files to custom types file

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,10 +3,12 @@ import "normalize.css";
 import Footer from "./Footer";
 import Nav from "./Nav";
 import GlobalStyles from "../styles/GlobalStyles";
+import Typography from "../styles/Typography";
 
 const Layout: FC = ({ children }) => (
   <div>
     <GlobalStyles />
+    <Typography />
     <Nav />
     {children}
     <Footer />
